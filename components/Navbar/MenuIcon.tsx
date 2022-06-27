ccd2a71d9bb66a264db9a2d5d72a25306aad8d28
baxy5 <bakos.soda@gmail.com>
2022-06-27 13:48:16 +0200

    cleanup

--- components/Navbar/MenuIcon.tsx
+++ components/Navbar/MenuIcon.tsx
@@ -5,7 +5,7 @@ import { HiMenuAlt1 } from "react-icons/hi";
 
 
 
-const MenuIcon = ({ isOpen, toggle }: { isOpen: boolean, toggle: Function }) => {
+const MenuIcon = ({ toggle }: { toggle: Function }) => {
   return (
     <div >
       <HiMenuAlt1 className="text-3xl lg:text-3xl cursor-pointer" onClick={() => toggle()} />
