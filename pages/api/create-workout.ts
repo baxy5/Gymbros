f37bc159205790ab1f1778dee53a9166f84bded3
baxy5 <bakos.soda@gmail.com>
2022-06-29 19:05:12 +0200

    database

--- pages/api/create-workout.ts
+++ pages/api/create-workout.ts
@@ -1,14 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import connect from "../../database/database";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // undefined Promise
-  const client = connect();
-  console.log(client);
-
   if (req.method === "POST") {
     let data = req.body;
 
