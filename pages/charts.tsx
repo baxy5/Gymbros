133dfd29398d3c99e54758a1704b867c19831249
baxy5 <bakos.soda@gmail.com>
2022-07-10 22:00:25 +0200

    visualizing data

--- pages/charts.tsx
+++ pages/charts.tsx
@@ -5,14 +5,18 @@ import Navbar from "../components/Navbar"
 import { LineChart, Line, Tooltip, XAxis, YAxis, ResponsiveContainer, AreaChart } from 'recharts';
 
 const charts = () => {
-    const [broProgress, setBroProgress] = useState([])
+    const [broProgress, setBroProgress] = useState({
+        data: []
+    })
 
     const readProgress = () => {
         fetch("/api/read-workout")
             .then((res) => res.json())
             .then((res) => {
-                console.log(res)
-                setBroProgress(res)
+                console.log(res[0].data)
+                setBroProgress({
+                    data: res[0].data
+                })
             })
     }
 
@@ -24,7 +28,9 @@ const charts = () => {
         <div className='flex w-full'>
             <Navbar />
             <div className='flex justify-around items-center w-full'>
-
+                <LineChart width={400} height={400} data={broProgress}>
+                    <Line type="monotone" dataKey="duration" stroke="#8884d8" />
+                </LineChart>
             </div>
         </div>
     )
