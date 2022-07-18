f90b490fd49951df4cc6a9e31ee8b59b30d672e1
baxy5 <bakos.soda@gmail.com>
2022-07-18 11:49:32 +0200

    fast save for breakfast

--- pages/charts.tsx
+++ pages/charts.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import Navbar from "../components/Navbar"
 
-import { LineChart, Line, Tooltip, XAxis, YAxis, ResponsiveContainer, AreaChart } from 'recharts';
+import { LineChart, Line, Tooltip, XAxis, YAxis, CartesianGrid, Legend } from 'recharts';
 
 const charts = () => {
     const [broProgress, setBroProgress] = useState({
@@ -24,12 +24,21 @@ const charts = () => {
         readProgress()
     }, [])
 
+    const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }, { name: 'Page B', uv: 300, pv: 3400, amt: 3400 }, { name: 'Page C', uv: 600, pv: 4400, amt: 1400 }];
+
     return (
         <div className='flex w-full'>
             <Navbar />
             <div className='flex justify-around items-center w-full'>
-                <LineChart width={400} height={400} data={broProgress}>
-                    <Line type="monotone" dataKey="duration" stroke="#8884d8" />
+                <LineChart width={730} height={250} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
                 </LineChart>
             </div>
         </div>
